0e803fd9b71ff67edd46c07ae043a50b75148cfc
Gessé Carlos <gesse.dev@outlook.com>
2022-01-14 15:23:13 -0300

    fixed lint issues in getTalker function

--- utils/talker.js
+++ utils/talker.js
@@ -3,11 +3,11 @@ const path = require('path');
 
 const getTalker = async () => {
   fs.readFile(path.join(__dirname, '..', 'talker.json'), 'utf-8')
-    .then((content) => content)
-    .catch((err) => {
-      console.error(err);
-      process.exit(1);
-    });
+  .then((content) => content)
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
 };
 
 module.exports = { getTalker };
