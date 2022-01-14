157e0c1f092091fb7cd2f717b4585e6b0c319fbb
Gessé Carlos <gesse.dev@outlook.com>
2022-01-14 16:15:29 -0300

    task 1 finished

--- utils/talker.js
+++ utils/talker.js
@@ -1,13 +1,14 @@
 const fs = require('fs/promises');
 const path = require('path');
 
-const getTalker = async () => {
-  fs.readFile(path.join(__dirname, '..', 'talker.json'), 'utf-8')
-  .then((content) => content)
-  .catch((err) => {
-    console.error(err);
+const getTalker = async (req, res) => {
+  try {
+    const result = await fs.readFile(path.join(__dirname, '..', 'talker.json'), 'utf-8');
+    res.status(200).json(JSON.parse(result));
+  } catch (error) {
+    console.error(error);
     process.exit(1);
-  });
+  }
 };
 
 module.exports = { getTalker };
