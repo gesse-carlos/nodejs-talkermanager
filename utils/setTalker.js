9f8ece43010e798747d4aeb024ee50d508f50a34
Gessé Carlos <gesse.dev@outlook.com>
2022-01-18 00:54:23 -0300

    i inserted the path module on setTalker

--- utils/setTalker.js
+++ utils/setTalker.js
@@ -1,8 +1,12 @@
 const fs = require('fs/promises');
+const path = require('path');
 
 module.exports = async (content) => {
   try {
-    return await fs.writeFile('./talker.json', JSON.stringify(content, null, 2));
+    return await fs.writeFile(
+      path.join(__dirname, '..', 'talker.json'),
+      JSON.stringify(content, null, 2),
+    );
   } catch (err) {
     console.error(err);
     process.exit(1);
