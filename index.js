3b082d6a693632ace78b047496a54f29afc7a62f
Gessé Carlos <gesse.dev@outlook.com>
2022-01-18 00:14:29 -0300

    i changed the name of createTalkerMiddleware to talkerMiddleware because i used him in all project

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const {
   validateAge,
   validateTalk,
   validateTalkKeys,
-} = require('./middlewares/createTalkerMiddleware');
+} = require('./middlewares/talkerMiddleware');
 
 const app = express();
 app.use(bodyParser.json());
