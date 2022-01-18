b7624eda2a2a68e76a11b2ee990137e3624c27f6
Gessé Carlos <gesse.dev@outlook.com>
2022-01-17 23:02:58 -0300

    update createTalkerMiddleware

--- middlewares/createTalkerMiddleware.js
+++ middlewares/createTalkerMiddleware.js
@@ -51,15 +51,15 @@ const validateTalkKeys = (req, res, next) => {
 
   const dateRegex = /^[\d]{2}\/[\d]{2}\/[\d]{4}$/;
 
-  if (!(Number.isInteger(rate)) || rate < 1 || rate > 5) {
-    return res.status(400).json({
-      message: 'O campo "rate" deve ser um inteiro de 1 à 5',
-    });
-  }
-
   if (!dateRegex.test(watchedAt)) {
     return res.status(400).json({
       message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"',
+    });
+  }
+
+  if (!(Number.isInteger(rate)) || rate < 1 || rate > 5) {
+    return res.status(400).json({
+      message: 'O campo "rate" deve ser um inteiro de 1 à 5',
     });
   }
 
